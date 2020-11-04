6e57f1f60bf19ce7b9e9330e22ce64bd89331b27
TheCollinZRusty <36445882+TheCollinZRusty@users.noreply.github.com>
2020-11-04 14:30:33 +0000

    Add to favorites logic completed; Dummy favorites page.

--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -5,8 +5,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../../globals/fontawesome";
 
 const MovieCard = props => {
+    const handleAddToFavorites = e => {
+        e.preventDefault()
+        props.buttonHandler(props.movie.id)  // new line
+      }
   return (
     <div className="col-sm-3">
+               <div className="card-footer">
+          <button type="button" className="btn w-100 btn-primary"
+                onClick={handleAddToFavorites}
+                >
+            Add to Favorites
+          </button>
+        </div>
       <div className="card  bg-white">
       <Link to={`/movies/${props.movie.id}`}>
         <img
