825678d020d533fa08bc231423135fc4917ce155
TheCollinZRusty <36445882+TheCollinZRusty@users.noreply.github.com>
2020-11-04 14:41:38 +0000

    Refactor: Template list page; HTTP API calls centralized

--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -3,18 +3,24 @@ import MovieHeader from "../components/headerMovie";
 import React, {useState, useEffect}  from "react";
 import MovieDetails from "../components/movieDetails";
 import "./moviePage.css";
+import {getMovie} from '../api/tmdb-api'
 
 const MoviePage = props => {
     const { id } = props.match.params
     const [movie, setMovie] = useState(null)
     useEffect(() => {
-      fetch(
-        `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
-      ).then(res => res.json())    
-      .then(movie => {
-        setMovie(movie);
-      })
-    }, [id])
+        getMovie(id).then(movie => {
+          setMovie(movie);
+        });
+      }, [id]);
+    // useEffect(() => {
+    //   fetch(
+    //     `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    //   ).then(res => res.json())    
+    //   .then(movie => {
+    //     setMovie(movie);
+    //   })
+    // }, [id])
   return (
     <>
       {movie ? (
