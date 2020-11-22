8def7a53141507f54828c5f48ca410549dbe43e7
Owen Collins <36445882+TheCollinZRusty@users.noreply.github.com>
2020-11-22 16:02:56 +0000

    Change site header

---
+++ src/contexts/moviesContext.js
@@ -0,0 +1,63 @@
+import React, { useEffect, createContext, useReducer } from "react";
+import { getMovies } from "../api/tmdb-api";
+
+export const MoviesContext = createContext(null);
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add-favorite":
+      return {
+        movies: state.movies.map((m) =>
+          m.id === action.payload.movie.id ? { ...m, favorite: true } : m
+        ),
+      };
+    case "load":
+      return { movies: action.payload.movies };
+      case "add-review":
+        return {
+          movies: state.movies.map((m) =>
+            m.id === action.payload.movie.id
+              ? { ...m, review: action.payload.review }
+              : m
+          ),
+        };
+      // Completed in next section
+      break;
+    default:
+      return state;
+  }
+};
+
+const MoviesContextProvider = (props) => {
+  const [state, dispatch] = useReducer(reducer, { movies: [] });
+
+  const addToFavorites = (movieId) => {
+    const index = state.movies.map((m) => m.id).indexOf(movieId);
+    dispatch({ type: "add-favorite", payload: { movie: state.movies[index] } });
+  };
+
+  const addReview = (movie, review) => {
+    dispatch({ type: "add-review", payload: { movie, review } });
+  }; 
+  useEffect(() => {
+    getMovies().then((movies) => {
+      dispatch({ type: "load", payload: { movies } });
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <MoviesContext.Provider
+      value={{
+        movies: state.movies,
+        favorites: state.favorites,
+        addToFavorites: addToFavorites,
+        addReview: addReview,
+      }}
+    >
+      {props.children}
+    </MoviesContext.Provider>
+  );
+};
+
+export default MoviesContextProvider;
--- src/index.js
+++ src/index.js
@@ -7,31 +7,31 @@ import MoviePage from './pages/movieDetailsPage'
 import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom"    // CHANGED
 import FavoriteMoviesPage from './pages/favoritesMoviesPage'       // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from './components/siteHeader'
+import UpcomingMoviesPage from "./pages/UpcomingMoviePage"
+import MoviesContextProvider from "./contexts/moviesContext";
+import GenresContextProvider from "./contexts/genresContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage'
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <div className="jumbotron">
-      <div className="container-fluid">
-        <ul className="navbar-nav text-black">
-          <li className="nav-item">
-            <Link className="nav-link " to="/">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link " to="/movies/favorites">
-              Favorites
-            </Link>
-          </li>
-        </ul>
-        <Switch>
+      <BrowserRouter>
+        <div className="jumbotron">
+          <SiteHeader />      {/* New Header  */}
+          <div className="container-fluid">
+          <MoviesContextProvider>     {/* NEW  */}
+          <GenresContextProvider>
+            <Switch>
+          <Route exact path="/reviews/form" component={AddMovieReviewPage} />
           <Route path="/reviews/:id" component={MovieReviewPage} />
           <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+          <Route exact path="/movies/Upcoming" component={UpcomingMoviesPage} />
           <Route path="/movies/:id" component={MoviePage} />
           <Route path="/" component={HomePage} />
           <Redirect from="*" to="/" />
         </Switch>
+        </GenresContextProvider>    {/* NEW */}
+        </MoviesContextProvider>     {/* NEW */}
       </div>
     </div>
   </BrowserRouter>
